a16d71157ed31dc9070d5f5014a61e169799fa6a
徐立辰 <306084800@qq.com>
2017-08-08 18:29:12 +0800

    article cache

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from blog import views
 from django.conf.urls import url
-from .feeds import ArticleRssFeed
+from django.views.decorators.cache import cache_page
+
 
 app_name = 'blog'
 urlpatterns = [
@@ -11,7 +12,7 @@ urlpatterns = [
     url(r'^contact/', views.contact, name='contact'),
     url(r'^full-width/', views.FullView.as_view(), name='blog'),
     # url(r'^article/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^article/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
+    url(r'^article/(?P<pk>[0-9]+)/$', cache_page(60 * 15)(views.PostDetailView.as_view()), name='detail'),
     # url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
     url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
--- xlc_blog/settings.py
+++ xlc_blog/settings.py
@@ -27,6 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# cache settings
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
 
 # Application definition
 
